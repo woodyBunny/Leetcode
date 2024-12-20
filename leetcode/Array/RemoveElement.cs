29976df4bf0823fa62fc177445224bfe279e1714
woodyBunny <woodyBunny30092@gmail.com>
2024-12-20 11:39:49 +0800

    feat: create RemoveElement

---
+++ leetcode/Array/RemoveElement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.Array
+{
+    internal class RemoveElement
+    {
+        public int inits(int[] nums, int val)
+        {
+            int answer = 0;
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != val)
+                {
+                    nums[answer] = nums[i];
+                    answer++;
+                }
+            }
+
+            return answer;
+        }
+    }
+}
