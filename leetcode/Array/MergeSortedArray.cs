75e8f0b82647eb4090f6e43190835842d63f2745
woodyBunny <woodyBunny30092@gmail.com>
2024-12-20 11:00:56 +0800

    update:mergeSortedArray

--- leetcode/Array/MergeSortedArray.cs
+++ leetcode/Array/MergeSortedArray.cs
@@ -10,28 +10,30 @@ namespace leetcode.Array
     {
         public int[] OptimizationSolution(int[] nums1, int m, int[] nums2, int n)
         {
-            for (int i = m + n - 1; i > -1; i--)
+            int p1 = m - 1;
+            int p2 = n - 1;
+            int p = m + n - 1;
+
+            while (p1 >= 0 && p2 >= 0)
             {
-                if (m == 0)
+                if (nums1[p1] > nums2[p2])
                 {
-                    nums1[i] = nums2[n - 1];
-                    n -= 1;
-                    continue;
-                }
-                else if (n == 0)
-                {
-                    break;
-                }
-                if (nums2[n - 1] >= nums1[m - 1])
-                {
-                    nums1[i] = nums2[n - 1];
-                    n -= 1;
+                    nums1[p] = nums1[p1];
+                    p1--;
                 }
                 else
                 {
-                    nums1[i] = nums1[m - 1];
-                    m -= 1;
+                    nums1[p] = nums2[p2];
+                    p2--;
                 }
+                p--;
+            }
+
+            while (p2 >= 0)
+            {
+                nums1[p] = nums2[p2];
+                p2--;
+                p--;
             }
 
             return nums1;
